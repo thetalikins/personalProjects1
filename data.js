870319738b57510e0936e31c6b724edd2edb3782
Theta Likins <thetalikins@Thetas-MacBook-Air.local>
2018-03-31 17:25:57 +1300

    basic html/css functioning

---
+++ data.js
@@ -0,0 +1,39 @@
+var shoppingList = [
+    {
+        id: 1,
+        name: apples,
+        type: fruit,
+        phrase: "crunchy",
+    },
+    {
+        id: 2,
+        name: banana,
+        type: fruit,
+        phrase: "creamy",
+    },
+    {
+        id: 3,
+        name: carrot,
+        type: vegetable,
+        phrase: "good for your eyeballs",
+
+    },
+    {
+        id: 4,
+        name: daisy,
+        type: flower,
+        phrase: "peace train is coming",
+    }
+    {
+        id: 5,
+        name: chia,
+        type: seed,
+        phrase: "makes excellent cereal",
+    }
+    {
+        id: 6,
+        name: Almond Milk,
+        type: beverage,
+        phrase: "for creaming coffee and making breakfast",
+    }
+]
